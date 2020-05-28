427121fa8b454b7dabf65377c5a281f4199df81d
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-28 16:33:46 -0700

    able to load mulitple images and save into folder

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask, render_template, Response, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from test import get_stats
-
 app = Flask(__name__)
+
+
+UPLOAD_FOLDER = '/Users/bryanevangelista/Documents/projects/flask-site/test'
+
+
+
+
 
 textOutput = {
     0 : "Proceed with Step 1",
@@ -11,45 +19,61 @@ textOutput = {
 }
 
 test=get_stats()
-@app.route('/', methods=['GET', 'POST'])
 
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # print(request.method)
     if request.method == 'POST':
-        if request.form.get('known') == 'known':
-            # call func
-            return render_template(
-                "index.html",
-                text_output=textOutput[1],
-                step1=False,
-                step2=True,
-                step3=False,
-                )
-        elif request.form.get('unknown') == 'unknown':
-            # pass # do something else
-            return render_template(
+        if 'images' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['images']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # print(request.files.getlist('images'))
+            for image in request.files.getlist('images'):
+                image.filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+            return redirect(request.url)
+
+    return render_template(
                 "index.html",
                 text_output=textOutput[2],
-                step1=False,
-                step2=False,
+                step1=True,
+                step2=True,
                 step3=True
                 )
-        elif request.form.get('start') == 'start':
-            # pass # do something else
-            return render_template(
-                "processing_images.html",
-                text_output=textOutput[3]
-                )
-        else:
-            # pass # unknown
-            return render_template("index.html")
-    else:
-    # return render_template("index.html")
-        return render_template(
-            "index.html",
-            step1=True,
-            text_output=textOutput[0],
-            )
+        # elif request.form.get('unknown') == 'unknown':
+        #     # pass # do something else
+        #     return render_template(
+        #         "index.html",
+        #         text_output=textOutput[2],
+        #         step1=False,
+        #         step2=False,
+        #         step3=True
+        #         )
+        # elif request.form.get('start') == 'start':
+        #     # pass # do something else
+        #     return render_template(
+        #         "processing_images.html",
+        #         text_output=textOutput[3]
+        #         )
+        # else:
+        #     # pass # unknown
+        #     return render_template("index.html")
+
 
 
 
--- test.py
+++ test.py
@@ -1,2 +1,12 @@
+import os
+
 def get_stats():
     return "gotten stats"
+
+TEST_DIR = "test"
+
+
+path = 'test'
+# test = os.listdir(path)
+
+test = os.listdir(TEST_DIR)
