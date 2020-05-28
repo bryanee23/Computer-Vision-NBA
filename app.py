a9139fe3c103b68d4097e4388fda946fed1a55e8
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-28 13:20:29 -0700

    directional flow added, buttons appear in sequence

--- app.py
+++ app.py
@@ -4,45 +4,52 @@ from test import get_stats
 app = Flask(__name__)
 
 textOutput = {
-    0 : "loading images",
-    1 : "loading unknown images",
-    2 : "processing started see below for results",
+    0 : "Proceed with Step 1",
+    1 : "loading images",
+    2 : "loading unknown images",
+    3 : "processing started see below for results",
 }
 
-directions = {
-    0 : "loading images",
-    1 : "loading unknown images",
-    2 : "processing started see below for results",
-}
 
 test=get_stats()
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
     # print(request.method)
     if request.method == 'POST':
         if request.form.get('known') == 'known':
             return render_template(
                 "index.html",
-                text_output=textOutput[0]
+                text_output=textOutput[1],
+                step1=False,
+                step2=True,
+                step3=False,
                 )
         elif request.form.get('unknown') == 'unknown':
             # pass # do something else
             return render_template(
                 "index.html",
-                text_output=textOutput[1]
+                text_output=textOutput[2],
+                step1=False,
+                step2=False,
+                step3=True
                 )
         elif request.form.get('start') == 'start':
             # pass # do something else
             return render_template(
                 "processing_images.html",
-                text_output=textOutput[2]
+                text_output=textOutput[3]
                 )
         else:
             # pass # unknown
             return render_template("index.html")
     else:
     # return render_template("index.html")
-        return render_template("index.html")
+        return render_template(
+            "index.html",
+            step1=True,
+            text_output=textOutput[0],
+            )
 
 
 
