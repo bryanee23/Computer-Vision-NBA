a6d4a5a0d3142fdfd0cf257a63823b100ff88999
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-28 16:49:00 -0700

    working step1 not working step2

--- app.py
+++ app.py
@@ -27,11 +27,9 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
+def upload_images(request):
     if request.method == 'POST':
+        print(request.files.getlist('images'))
         if 'images' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -42,37 +40,51 @@ def index():
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
-            # print(request.files.getlist('images'))
+
             for image in request.files.getlist('images'):
                 image.filename = secure_filename(image.filename)
                 image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
             return redirect(request.url)
 
-    return render_template(
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # print(request.method)
+    if request.method == 'POST':
+        if request.form.get('known') == 'known':
+            upload_images(request)
+            return render_template(
+                "index.html",
+                text_output=textOutput[1],
+                step1=False,
+                step2=True,
+                step3=False,
+                )
+        elif request.form.get('unknown') == 'unknown':
+            upload_images(request)
+            return render_template(
                 "index.html",
                 text_output=textOutput[2],
-                step1=True,
-                step2=True,
+                step1=False,
+                step2=False,
                 step3=True
                 )
-        # elif request.form.get('unknown') == 'unknown':
-        #     # pass # do something else
-        #     return render_template(
-        #         "index.html",
-        #         text_output=textOutput[2],
-        #         step1=False,
-        #         step2=False,
-        #         step3=True
-        #         )
-        # elif request.form.get('start') == 'start':
-        #     # pass # do something else
-        #     return render_template(
-        #         "processing_images.html",
-        #         text_output=textOutput[3]
-        #         )
-        # else:
-        #     # pass # unknown
-        #     return render_template("index.html")
+        elif request.form.get('start') == 'start':
+            # pass # do something else
+            return render_template(
+                "processing_images.html",
+                text_output=textOutput[3]
+                )
+        else:
+            # pass # unknown
+            return render_template("index.html")
+    else:
+    # return render_template("index.html")
+        return render_template(
+            "index.html",
+            step1=True,
+            text_output=textOutput[0],
+            )
 
 
 
