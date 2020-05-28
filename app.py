33e44ce8594d6827cfec2901cf0dba68f6043e67
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-27 22:34:42 -0700

    processing images.html added

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from test import get_stats
 
 app = Flask(__name__)
 
+textOutput = {
+    0 : "loading images",
+    1 : "loading unknown images",
+    2 : "processing started see below for results",
+}
 
 test=get_stats()
 @app.route('/', methods=['GET', 'POST'])
@@ -10,14 +15,26 @@ def index():
     # print(request.method)
     if request.method == 'POST':
         if request.form.get('known') == 'known':
-            return render_template("index.html", test=test)
+            return render_template(
+                "index.html",
+                text_output=textOutput[0]
+                )
         elif request.form.get('unknown') == 'unknown':
             # pass # do something else
-            return render_template("index.html", test="unknown hit")
+            return render_template(
+                "index.html",
+                text_output=textOutput[1]
+                )
+        elif request.form.get('start') == 'start':
+            # pass # do something else
+            return render_template(
+                "processing_images.html",
+                text_output=textOutput[2]
+                )
         else:
             # pass # unknown
             return render_template("index.html")
-    elif request.method == 'GET':
+    else:
     # return render_template("index.html")
         return render_template("index.html")
 
