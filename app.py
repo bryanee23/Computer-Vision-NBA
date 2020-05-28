e9aea4a6b4384ea9943c1d3c28a4d9b28741b623
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-27 22:05:09 -0700

    added 2 buttons that work

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
+from test import get_stats
 
 app = Flask(__name__)
 
 
-@app.route('/')
+test=get_stats()
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # print(request.method)
+    if request.method == 'POST':
+        if request.form.get('known') == 'known':
+            return render_template("index.html", test=test)
+        elif request.form.get('unknown') == 'unknown':
+            # pass # do something else
+            return render_template("index.html", test="unknown hit")
+        else:
+            # pass # unknown
+            return render_template("index.html")
+    elif request.method == 'GET':
+    # return render_template("index.html")
+        return render_template("index.html")
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
-    
-print('here')
