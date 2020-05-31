1aebf5df2d5c26bd1abe7a14a6632239ee792cb6
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-31 14:18:33 -0700

    images folder was deleted for some reason, re-added folder

--- directory.py
+++ directory.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
+# app = Flask(__name__)
+# app.secret_key = os.urandom(24)
 
 # directory setup, opencv border settings
 ROOT_DIR = os.getcwd()
